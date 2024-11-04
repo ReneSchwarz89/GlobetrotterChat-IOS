3dd06e32f7e5429b1f1a49ee74e989d57c57f781
reneschwarz <rene.schwarz1989@gmail.com>
2024-11-04 21:09:12 +0100

    -fix bug at isValidEmail

--- GlobetrotterChat/GlobetrotterChat/Landing/AuthViewModel.swift
+++ GlobetrotterChat/GlobetrotterChat/Landing/AuthViewModel.swift
@@ -78,9 +78,9 @@ enum AuthError: LocalizedError {
     }
     
     private func isValidEmail(_ email: String) -> Bool {
-        let emailRegEx = "[A-Z0--z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: email)
+            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+            let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+            return emailPred.evaluate(with: email)
     }
     
     private func clearPrivateInfo() {
