77cf7746a60cb01a99bd86dc2805b350040b2f73
reneschwarz <rene.schwarz1989@gmail.com>
2024-11-04 18:29:47 +0100

    -update chatview ui -clean white space

--- GlobetrotterChat/GlobetrotterChat/Views/ChatRoom/ChatGroupsView.swift
+++ GlobetrotterChat/GlobetrotterChat/Views/ChatRoom/ChatGroupsView.swift
@@ -11,102 +11,101 @@ import Observation
 struct ChatGroupsView: View {
     @State var viewModel: ChatGroupsViewModel
     @State private var tabBar: UITabBar! = nil
-
+    
     var body: some View {
         NavigationStack {
-            
+            VStack{
                 List(viewModel.chatGroups) { chatGroup in
                     NavigationLink(destination: ChatView(chatGroup: chatGroup)
                         .onAppear { self.tabBar.isHidden = true }
                         .onDisappear { self.tabBar.isHidden = false }) {
-                        HStack {
-                            if chatGroup.isGroup {
-                                if let url = URL(string: chatGroup.groupPictureURL ?? "") {
-                                    AsyncImage(url: url) { phase in
-                                        switch phase {
-                                        case .empty:
-                                            ProgressView()
-                                        case .success(let image):
-                                            image.resizable()
-                                                .aspectRatio(contentMode: .fill)
-                                                .frame(width: 50, height: 50)
-                                                .clipShape(Circle())
-                                        case .failure:
-                                            Circle()
-                                                .fill(Color.gray)
-                                                .frame(width: 50, height: 50)
-                                        @unknown default:
-                                            EmptyView()
-                                        }
-                                    }
-                                } else {
-                                    Circle()
-                                        .fill(Color.gray)
-                                        .frame(width: 50, height: 50)
-                                }
-                                Text(chatGroup.groupName ?? "Group")
-                                    .font(.headline)
-                            } else {
-                                if let contactID = chatGroup.participants.first(where: { $0.id != viewModel.uid })?.id {
-                                    if let contact = viewModel.possibleContacts.first(where: { $0.contactID == contactID }) {
-                                        if let url = URL(string: contact.profileImage ?? "") {
-                                            AsyncImage(url: url) { phase in
-                                                switch phase {
-                                                case .empty:
-                                                    ProgressView()
-                                                case .success(let image):
-                                                    image.resizable()
-                                                        .aspectRatio(contentMode: .fill)
-                                                        .frame(width: 50, height: 50)
-                                                        .clipShape(Circle())
-                                                case .failure:
-                                                    Circle()
-                                                        .fill(Color.gray)
-                                                        .frame(width: 50, height: 50)
-                                                @unknown default:
-                                                    EmptyView()
-                                                }
+                            HStack {
+                                if chatGroup.isGroup {
+                                    if let url = URL(string: chatGroup.groupPictureURL ?? "") {
+                                        AsyncImage(url: url) { phase in
+                                            switch phase {
+                                            case .empty:
+                                                ProgressView()
+                                            case .success(let image):
+                                                image.resizable()
+                                                    .aspectRatio(contentMode: .fill)
+                                                    .frame(width: 50, height: 50)
+                                                    .clipShape(Circle())
+                                            case .failure:
+                                                Circle()
+                                                    .fill(Color.gray)
+                                                    .frame(width: 50, height: 50)
+                                            @unknown default:
+                                                EmptyView()
                                             }
-                                        } else {
-                                            Circle()
-                                                .fill(Color.gray)
-                                                .frame(width: 50, height: 50)
                                         }
-                                        Text(contact.nickname)
-                                            .font(.headline)
+                                    } else {
+                                        Circle()
+                                            .fill(Color.gray)
+                                            .frame(width: 50, height: 50)
+                                    }
+                                    Text(chatGroup.groupName ?? "Group")
+                                        .font(.headline)
+                                } else {
+                                    if let contactID = chatGroup.participants.first(where: { $0.id != viewModel.uid })?.id {
+                                        if let contact = viewModel.possibleContacts.first(where: { $0.contactID == contactID }) {
+                                            if let url = URL(string: contact.profileImage ?? "") {
+                                                AsyncImage(url: url) { phase in
+                                                    switch phase {
+                                                    case .empty:
+                                                        ProgressView()
+                                                    case .success(let image):
+                                                        image.resizable()
+                                                            .aspectRatio(contentMode: .fill)
+                                                            .frame(width: 50, height: 50)
+                                                            .clipShape(Circle())
+                                                    case .failure:
+                                                        Circle()
+                                                            .fill(Color.gray)
+                                                            .frame(width: 50, height: 50)
+                                                    @unknown default:
+                                                        EmptyView()
+                                                    }
+                                                }
+                                            } else {
+                                                Circle()
+                                                    .fill(Color.gray)
+                                                    .frame(width: 50, height: 50)
+                                            }
+                                            Text(contact.nickname)
+                                                .font(.headline)
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
                 }
-            
-            .navigationTitle("Chats")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    HStack {
-                        Button(action: {
-                            viewModel.isAddChatGroupSheetPresented = true
-                        }) {
-                            Image(systemName: "plus")
+                
+                .navigationTitle("Chats")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        HStack {
+                            Button(action: {
+                                viewModel.isAddChatGroupSheetPresented = true
+                            }) {
+                                Image(systemName: "plus")
+                            }
                         }
                     }
                 }
-            }
-            .sheet(isPresented: $viewModel.isAddChatGroupSheetPresented) {
-                AddChatGroupSheet(viewModel: viewModel)
-            }
-            .background(TabBarAccessor { tabbar in
-                self.tabBar = tabbar
-            })
-            .onAppear {
-                viewModel.isTabBarVisible = true
+                .sheet(isPresented: $viewModel.isAddChatGroupSheetPresented) {
+                    AddChatGroupSheet(viewModel: viewModel)
+                }
+                .background(TabBarAccessor { tabbar in
+                    self.tabBar = tabbar
+                })
+                .onAppear {
+                    viewModel.isTabBarVisible = true
+                }
             }
         }
     }
 }
-
-
 
 #Preview {
     ChatGroupsView(viewModel: ChatGroupsViewModel())
@@ -114,7 +113,7 @@ struct ChatGroupsView: View {
 
 struct TabBarAccessor: UIViewControllerRepresentable {
     var callback: (UITabBar) -> Void
-
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<TabBarAccessor>) -> UIViewController {
         let viewController = UIViewController()
         DispatchQueue.main.async {
@@ -124,6 +123,6 @@ struct TabBarAccessor: UIViewControllerRepresentable {
         }
         return viewController
     }
-
+    
     func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<TabBarAccessor>) {}
 }
--- GlobetrotterChat/GlobetrotterChat/Views/ChatView.swift
+++ GlobetrotterChat/GlobetrotterChat/Views/ChatView.swift
@@ -11,104 +11,106 @@ struct ChatView: View {
     @State var viewModel: ChatViewModel
     let chatGroup: ChatGroup
     @State private var scrollViewProxy: ScrollViewProxy? = nil
-
+    
     init(chatGroup: ChatGroup) {
         self.chatGroup = chatGroup
         _viewModel = State(wrappedValue: ChatViewModel(manager: FirebaseMessagesManager(), chatGroup: chatGroup))
     }
-
+    
     var body: some View {
-        VStack {
-            HStack {
-                AsyncImage(url: URL(string: chatGroup.groupPictureURL ?? "")) { image in
-                    image.resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 50, height: 50)
-                        .clipShape(Circle())
-                } placeholder: {
-                    Circle()
-                        .fill(Color.gray)
-                        .frame(width: 50, height: 50)
-                }
-                Text(chatGroup.groupName ?? "Chat Group Details")
-                    .font(.title)
-                    .padding()
-                Spacer()
-            }
-            .padding(.horizontal, 16)
-            
-            ScrollViewReader { proxy in
-                ScrollView {
-                    VStack {
-                        ForEach(viewModel.messages) { message in
-                            HStack {
-                                if message.senderId == viewModel.uid {
-                                    Spacer()
-                                    Text(message.text)
-                                        .padding()
-                                        .background(Color.arcticBlue)
-                                        .foregroundColor(.white)
-                                        .cornerRadius(10)
-                                        .frame(maxWidth: 250, alignment: .trailing)
-                                } else {
-                                    // Hole die `targetLanguageCode` des Empfängers
-                                    let targetLanguageCode = chatGroup.participants.first(where: { $0.id == viewModel.uid })?.targetLanguageCode ?? "EN"
-                                    Text(message.translations[targetLanguageCode] ?? message.text)
-                                        .padding()
-                                        .background(Color.arcticBlue.opacity(0.5))
-                                        .foregroundColor(.white)
-                                        .cornerRadius(10)
-                                        .frame(maxWidth: 250, alignment: .leading)
-                                    Spacer()
+        NavigationView {
+            VStack {
+                ScrollViewReader { proxy in
+                    ScrollView {
+                        VStack {
+                            ForEach(viewModel.messages) { message in
+                                HStack {
+                                    if message.senderId == viewModel.uid {
+                                        Spacer()
+                                        Text(message.text)
+                                            .padding()
+                                            .background(Color.arcticBlue)
+                                            .foregroundColor(.white)
+                                            .cornerRadius(10)
+                                            .frame(maxWidth: 250, alignment: .trailing)
+                                    } else {
+                                        let targetLanguageCode = chatGroup.participants.first(where: { $0.id == viewModel.uid })?.targetLanguageCode ?? "EN"
+                                        Text(message.translations[targetLanguageCode] ?? message.text)
+                                            .padding()
+                                            .background(Color.arcticBlue.opacity(0.5))
+                                            .foregroundColor(.white)
+                                            .cornerRadius(10)
+                                            .frame(maxWidth: 250, alignment: .leading)
+                                        Spacer()
+                                    }
+                                }
+                                .padding(message.senderId == viewModel.uid ? .leading : .trailing, 50)
+                                .padding(.vertical, 5)
+                            }
+                            .padding(.horizontal, 12)
+                        }
+                        .onChange(of: viewModel.messages.count) { _, newValue in
+                            if let lastMessage = viewModel.messages.last {
+                                withAnimation {
+                                    proxy.scrollTo(lastMessage.id, anchor: .bottom)
                                 }
                             }
-                            .padding(message.senderId == viewModel.uid ? .leading : .trailing, 50)
-                            .padding(.vertical, 5)
-                        }
-                        .padding(.horizontal, 12)
-                    }
-                    .onChange(of: viewModel.messages.count) { _, newValue in
-                        if let lastMessage = viewModel.messages.last {
-                            withAnimation {
-                                proxy.scrollTo(lastMessage.id, anchor: .bottom)
-                            }
                         }
                     }
+                    .onAppear {
+                        self.scrollViewProxy = proxy
+                    }
                 }
-                .onAppear {
-                    self.scrollViewProxy = proxy
-                }
-            }
-            
-            HStack {
-                TextField("Nachricht eingeben...", text: $viewModel.newMessageText)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(10)
                 
-                Button(action: {
-                    viewModel.sendMessage(chatGroup: chatGroup)
-                }) {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(viewModel.newMessageText.isEmpty ? Color.gray : Color.arcticBlue) // Hintergrundfarbe ändern
-                        .frame(width: 52, height: 52) // Größe des Rechtecks
-                        .overlay(
-                            Image(systemName: "paperplane.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 24, height: 24)
-                                .foregroundColor(Color.white)
-                        )
+                HStack {
+                    TextField("Nachricht eingeben...", text: $viewModel.newMessageText)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                    
+                    Button(action: {
+                        viewModel.sendMessage(chatGroup: chatGroup)
+                    }) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(viewModel.newMessageText.isEmpty ? Color.gray : Color.arcticBlue)
+                            .frame(width: 52, height: 52)
+                            .overlay(
+                                Image(systemName: "paperplane.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 24, height: 24)
+                                    .foregroundColor(Color.white)
+                            )
+                    }
+                    .disabled(viewModel.newMessageText.isEmpty)
                 }
-                .disabled(viewModel.newMessageText.isEmpty) // Deaktiviert den Button, wenn das Textfeld leer ist
+                .padding()
             }
-            .padding()
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack {
+                        AsyncImage(url: URL(string: chatGroup.groupPictureURL ?? "")) { image in
+                            image.resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                        } placeholder: {
+                            Circle()
+                                .fill(Color.gray)
+                                .frame(width: 40, height: 40)
+                        }
+                        Text(chatGroup.groupName ?? "Chat Group Details")
+                            .font(.title2)
+                            .padding(.leading, 8)
+                    }
+                }
+            }
         }
-        .ignoresSafeArea(edges: .bottom)
+        .ignoresSafeArea(.keyboard)
     }
 }
 
 #Preview {
     ChatView(chatGroup: ChatGroup(id: "1", participants: [], isGroup: true, admin: nil, groupName: "Test Group", groupPictureURL: nil))
 }
-
